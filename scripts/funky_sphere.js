8006568e392a2266c8560f73661cccd7f2ac2460
Hans Elliott <hanselliott61@gmail.com>
2023-06-05 18:06:20 -0700

    improve

--- scripts/funky_sphere.js
+++ scripts/funky_sphere.js
@@ -10,7 +10,7 @@ function randInt(min, max) {
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 5);
+camera.position.set(0, 0, 12);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
